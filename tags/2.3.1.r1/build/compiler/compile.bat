3f5173a39e5f886ded66ae1c38f5d9bb19e1cf84
Jonno.5000@gmail.com <Jonno.5000@gmail.com@e53694a3-6ed1-87d4-8bed-b3b41ca0c10c>
2013-12-14 02:28:50 +0000

    Release cleanup.

--- tags/2.3.1.r1/build/compiler/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-builder.exe
-
-setlocal
-set PATH=%PATH%;../backend_win/builder/bin/Release/
-
-echo Running YUI Compressor...
-java.exe -jar yuicompressor-2.4.8pre.jar --nomunge -o ../jsbox2d.min.js ../jsbox2d.js
-endlocal
